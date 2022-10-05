42d9ccc3a0805c373022eecfa563307dc0ea62eb
Keks <keks@htmlacademy.ru>
2022-10-05 18:07:27 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,20 +1,24 @@
 // функция взята с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min < 0 || max < 0 && max <= min && max === min) {
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
+  if (max <= min) {
+    return NaN;
+  }
+  if (max === min) {
     return NaN;
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 getRandomIntInclusive(5, 10);
 
 
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 checkStringLength('Hello world!', 15);
