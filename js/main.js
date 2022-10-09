1bbc38cce844fc540a4801e4838dbfe2fe7373f0
Roman Grachev <romagra92@gmail.com>
2022-10-09 19:09:24 +0500

    Сгенерировал моковые данные

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ getRandomIntInclusive(5, 10);
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 checkStringLength('Hello world!', 15);
+
+const MAX_POST_AMOUNT = 25;
+const MIN_LIKES_AMOUNT = 15;
+const MAX_LIKES_AMOUNT = 200;
+const MAX_COMMENTS_AMOUNT = 200;
+const posts = [];
+const postDescriptions = ['фото кота', 'фото хомяка', 'фото собаки', 'фото белки', 'фото природы', 'фото квартиры', 'фото автомобиля'];
+
+const createPost = (count) => ({
+  id: count,
+  url: `photos/${count}.jpg`,
+  description: postDescriptions[getRandomIntInclusive(0, postDescriptions.length - 1)],
+  likes: getRandomIntInclusive(MIN_LIKES_AMOUNT, MAX_LIKES_AMOUNT),
+  comments: getRandomIntInclusive(0, MAX_COMMENTS_AMOUNT)
+});
+
+for (let i = 1; i <= MAX_POST_AMOUNT; i++) {
+  posts.push(createPost(i));
+}
+
